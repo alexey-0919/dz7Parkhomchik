3d66369ffa0b36aec7b5155d5ac1df66250cd678
alehey <alexey.p.svyz@mail.ru>
2023-02-21 18:19:34 +0700

    add task

---
+++ task50/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+int InputNumber()
+{
+    Console.WriteLine("Input number");
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+
+int m = InputNumber();
+int n = InputNumber();
+
+int[,] array = new int[m, m];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToInt32(new Random().Next(15));
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "   ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+Console.WriteLine();
+
+
+int z = InputNumber();
+int y = InputNumber();
+
+
+
+if (z < m && y < n)
+    Console.WriteLine(array[z,y]);
+else
+    Console.WriteLine($"{z} {y} -> такого числа в массиве нет");
+
+   
---
+++ task52/Program.cs
@@ -0,0 +1,65 @@
+﻿/* Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+int InputNumber()
+{
+    Console.WriteLine("Input number");
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+
+int m = InputNumber();
+int n = InputNumber();
+
+int[,] array = new int[m, m];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToInt32(new Random().Next(15));
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "   ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+Console.WriteLine();
+
+double sum = 0;
+double SrArif = 1;
+
+for (int j = 0; j < array.GetLength(1); j++)
+    {
+         
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+            
+            sum = sum + array[i,j];
+        }
+        SrArif = sum / array.GetLength(0);
+        Console.Write($"{Math.Round(SrArif, 2)}  ");
+        sum = 0;
+    }
+    Console.ReadLine();
